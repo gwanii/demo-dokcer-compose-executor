d53958a8adee11e867e3006a2cca66923e2311d1
ayakashi <gwanmax@gmail.com>
2016-08-22 19:19:58 +0800

    first commit

---
+++ create_app.sh
@@ -0,0 +1,1 @@
+curl -H 'Content-Type: application/json' -X POST -d @demo/demo.json http://192.168.99.103:8080/v2/apps
---
+++ demo/docker-compose-executor.sh
@@ -0,0 +1,2 @@
+COMPOSE_JAR_NAME=/root/docker-compose-executor/target/docker-compose-executor_0.0.1.jar
+java -jar ${COMPOSE_JAR_NAME}
